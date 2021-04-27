2a9093941b12c41d9614d9ffdc19bd6cae32c16e
Alexander Kovzhut <akovzhut@icloud.com>
2021-04-28 00:34:22 +0300

    Update Contents.swift

--- KovzhutAV_HW1.5.playground/Contents.swift
+++ KovzhutAV_HW1.5.playground/Contents.swift
@@ -1,19 +1,96 @@
 import UIKit
 
+
 //1
 
-let dictionary = ["Cалават Юлаев":"", "Авангард":"", "Ак Барс":"", ]
+
+let gameResults = [
+    "Cалават Юлаев": ["3:6" , "5:5" , "N/A"],
+    "Авангард": ["2:1"],
+    "Ак Барс": ["3:3" , "1:2"]
+]
+
+for (team, result) in gameResults {
+    for match in result {
+        print("Игра с \(team) - \(match)")
+    }
+}
 
 
 //2
 
-func wallet (summOfMoney: Int) -> Int {
-    return summOfMoney
+
+func wallet (for cash: Int...) -> Int {
+    var sum = 0
+    
+    for currentCash in cash {
+        sum += currentCash
+    }
+    
+    return sum
 }
 
-wallet(summOfMoney: 1)
+wallet(for: 50, 10, 100, 200, 5000, 1000, 2000, 500)
 
 
-//3
+
+//3.1
+
+
+func evenOrUneven (number: Int) -> Bool {
+    number % 2 == 0 ? true : false
+}
+
+evenOrUneven(number: 1)
+evenOrUneven(number: 2)
+
+
+//3.2
+
+
+func divisionByThree (number: Int) -> Bool {
+    number % 3 == 0 ? true : false
+}
+
+divisionByThree(number: 299)
+divisionByThree(number: 300)
+
+
+//3.3
+
+
+func returnArrayLength (begin x: Int, end y: Int) -> [Int] {
+    var length: [Int] = []
+    
+    for currentLength in x...y {
+        length.append(currentLength)
+    }
+    
+    return length
+}
+
+returnArrayLength (begin: 1, end: 9)
+
+
+//3.4
+
+let someArray = returnArrayLength(begin: 1, end: 100)
+
+
+//3.5
+
+func filter (for array: [Int]) -> [Int] {
+    var evenNumbers: [Int] = []
+    
+    for unit in array {
+        if evenOrUneven(number: unit) {
+            evenNumbers.append(unit)
+        }
+    }
+    
+    return evenNumbers
+}
+
+filter(for: someArray)
 
 
