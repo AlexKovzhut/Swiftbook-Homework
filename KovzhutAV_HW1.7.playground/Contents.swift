0e40566648cce17d3773b9540913fafbc728465b
Alexander Kovzhut <akovzhut@icloud.com>
2021-05-03 23:58:42 +0300

    initial commit

---
+++ KovzhutAV_HW1.7.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+//1.1
+
+enum CalculationType {
+    
+}
+
+//1.2
+
+
+
+//1.3
+
+
+
+//2.1
+
+enum CurrencyUnit {
+    
+}
+
+//2.2
+
+
+
+//2.3
+
+
+
+//3.1
+
+struct ChessPlayer {
+    let name: String
+    let wins: Int
+    let description: String
+    
+    private func addWins() {
+        0
+    }
+}
+
+//3.2
+
+let player = ChessPlayer (name: "", wins: 1, description: "")
+
